66377b000e8a59844ba3822eaa7080755f15762f
Rodolfo Pereira <phinpho@gmail.com>
2024-11-28 08:22:56 +0100

    fix(markdown): re-export the react-markdown types

--- packages/markdown/src/index.ts
+++ packages/markdown/src/index.ts
@@ -1,4 +1,12 @@
 import { lazy } from 'react'
 
+export type {
+  AllowElement,
+  Components,
+  ExtraProps,
+  Options,
+  UrlTransform,
+} from 'react-markdown'
+
 export type { MarkdownProps } from './Markdown'
 export const Markdown = lazy(() => import('./Markdown'))
